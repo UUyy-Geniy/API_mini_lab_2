60c523ebbae83af882391f3f1785e988c7fffccb
UUyy-Geniy <jekapeheka@gmail.com>
2023-11-15 20:05:37 +0300

    add sign in form

--- js/main.js
+++ js/main.js
@@ -1,13 +1,4 @@
-import {setFormValue, submitSignUpForm, validateEmail, validatePassword} from "./utils.js"
-
-
-////// ДЕМОНСТРАЦИОННЫЙ УЧАСТОК КОДА. На оценку не влияет, исключительно для саморазвития.
-
-// Предлагаю "поиграться" с частями кода ниже, чтобы познакомиться с JS
-// Получаем элемент и меняем его класс, который определеён в библиотеке стилей materialize
-const password = document.getElementById('password');
-password.classList.add("valid")
-password.classList.remove("valid")
+import {setFormValue, submitSignUpForm, validateEmail, validatePassword, validateRepPassword} from "./utils.js"
 
 // В браузере можно посмотреть, что из себя представляет документ
 // (CTRL+SHIFT+i для открытия консоли и открыть вкладку "консоль", туда будет залогированно значение)
@@ -34,7 +25,10 @@ console.log(document)
 const first_name_id = 'first_name'
 const last_name_id = 'last_name'
 const password_id = 'password'
+const sign_in_password_id = 'sign_in_password'
+const rep_password_id = 'password-repeat'
 const email_id = 'email'
+const sign_in_email_id = 'sign_in_email'
 
 const sign_in_link_id = 'sign_in_link'
 const sign_up_form_id = 'sign_up_form'
@@ -43,16 +37,27 @@ const sign_up_btn_id = 'sign_up_btn'
 const sign_in_form_id = 'sign_in_form'
 
 
+const password = document.getElementById('password');
+const rep_password = document.getElementById('password-repeat');
+const sign_in_password = document.getElementById('sign_in_password');
+const sign_in_email = document.getElementById('sign_in_email');
+
 // Получаем элемент DOM-дерева по id и присваиваем значение аттрибуту oninput
 // oninput вызывается с параметром "event" каждый раз, когда ввод меняется
 // Значение, которое мы присваеваем этому аттрибуту - это функция, определённая в стрелочном стиле
 // Гуглить по тегам "события JS", "onchange/oninput HTML", "стрелочные функции JS", ...
 
 const first_name = document.getElementById(first_name_id);
-first_name.oninput = (e) => setFormValue(first_name_id, e.target.value)  // Установить значение без валидации
+first_name.oninput = (e) => setFormValue(first_name_id, e.target)  // Установить значение без валидации
 
 const email = document.getElementById(email_id);
-email.oninput = (e) => setFormValue(email_id, e.target.value, validateEmail) // Установить значение с валидацией
+email.oninput = (e) => setFormValue(email_id, e.target, validateEmail) // Установить значение с валидацией
+
+
+password.oninput = (e) => setFormValue(password_id,e.target, validatePassword)
+rep_password.oninput = (e) => setFormValue(rep_password_id,e.target, validateRepPassword)
+sign_in_password.oninput = (e) => setFormValue(sign_in_password_id,e.target, validatePassword)
+sign_in_email.oninput = (e) => setFormValue(sign_in_email_id, e.target, validateEmail)
 
 
 
